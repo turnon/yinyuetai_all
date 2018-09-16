dabb9012514e06cc1557cf9df20191c72b471785
ken <block24block@gmail.com>
2018-09-16 23:08:17 +0800

    use ateles

--- yinyuetai_all.js
+++ yinyuetai_all.js
@@ -1,49 +1,31 @@
-/**
- * Created by turnon on 2018/9/9.
- */
 // ==UserScript==
 // @name         yinyuetai-all
 // @namespace    https://github.com/turnon/yinyuetai_all
-// @version      0.0.3
+// @version      0.0.4
 // @description  yinyuetai
 // @author       block24block@gmail.com
 // @match       *://www.yinyuetai.com/fanclub/*
 // @grant        none
-// @require 	 https://cdn.bootcss.com/jquery/3.2.1/jquery.js
+// @require 	 https://greasyfork.org/scripts/372188-ateles/code/ateles.js?version=629487
 // ==/UserScript==
-window.$$$ = jQuery.noConflict(true);
-
-(function () {
-  function start() {
-    var $page_count = $$$(".page-nav span:last-child");
-    var page_count = parseInt($page_count.text().replace(/[^0-9]/g, ""));
-
-    var page_loaders = [];
-    var base_url = window.location.href;
-    for (var i = 2; i <= page_count; i++) {
-      var next_page = base_url + "/" + i;
-      page_loaders.push($$$.ajax({
-        url: next_page
-      }));
-    }
-
-    async function append_page(page_loader) {
-      var data = await page_loader;
-      var $mod = $$$(data).find(".mod");
+Ateles(['jq', 'page_loader'], function (jq, page_loader) {
+  page_loader(jq, {
+    page_count: function ($) {
+      var $page_count = $(".page-nav span:last-child");
+      return parseInt($page_count.text().replace(/[^0-9]/g, ""));
+    },
+    next_page: function (n) {
+      return window.location.href + '/' + n;
+    },
+    append_page: function (data, $) {
+      var $mod = $(data).find(".mod");
       $mod.find(".thread_list").remove();
-      $$$(".mod:last-child").after($mod);
+      $(".mod:last-child").after($mod);
+    },
+    button: function ($) {
+      var $btn = $('<span>load all</span>');
+      $(".page-nav span:last-child").before($btn);
+      return $btn;
     }
-
-    (async function () {
-      for (loader of page_loaders) {
-        await append_page(loader);
-      }
-    })();
-
-    $all.remove();
-  }
-
-  var $all = $$$('<a href="#">load all</a>');
-  $$$(".page-nav span:last-child").before($all);
-  $all.click(start);
-})();
+  })
+})
